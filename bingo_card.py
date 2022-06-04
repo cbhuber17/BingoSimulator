3f6608cb2f365bd93df2d273628a8e71775bb41f
Colin Huber <cbhuber@gmail.com>
2022-06-04 14:41:49 -0600

    Heavy refactoring.

--- bingo_card.py
+++ bingo_card.py
@@ -36,11 +36,11 @@ class BingoCard:
         if free_cell:
             self.bingo_card[2][2] = [0, True]
 
-        self.marked_bingo_card = copy.deepcopy(self.bingo_card)
+        # TODO: May need to remove marked card
 
     # ------------------------------------------------------------------------
 
-    def print_bingo_card(self, card):
+    def print_bingo_card(self):
 
         print('------------------------')
         print('|B | |I | |N | |G | |O |')
@@ -51,6 +51,78 @@ class BingoCard:
             if i > 0:
                 print('\n------------------------')
             for j in range(0, CARD_LENGTH):
-                print('|{:2}|'.format(card[i][j][True]), end=" ")
+                print('|{:2}|'.format(self.bingo_card[i][j][True]), end=" ")
 
         print('\n------------------------\n\n')
+
+    # ------------------------------------------------------------------------
+
+    def _check_diagonal_bingo(self, diag_num, stats):
+
+        if diag_num not in [1, 2]:
+            raise ValueError(f'Diagonal Number must be 1 or 2 in check_diagonal_bingo(): {diag_num}.')
+
+        if diag_num == 1:
+            for i in range(0, CARD_LENGTH):
+                if not self.bingo_card[i][i][True]:
+                    return False
+
+        else:
+            for i in range(0, CARD_LENGTH):
+                if not self.bingo_card[i][CARD_LENGTH - 1 - i][True]:
+                    return False
+
+        stats.num_diag_bingo[diag_num - 1] += 1
+        return True
+
+    # ------------------------------------------------------------------------
+
+    def _check_line_bingo(self, axis, line_num, stats):
+
+        if axis not in [0, 1]:
+            raise ValueError(f'Axis is not 0 (row) or 1 (column): {axis}.')
+
+        if line_num not in list(range(0, CARD_LENGTH)):
+            raise ValueError(f'Line number for axis {axis} must be 0-4 in check_line_bingo(): {line_num}.')
+
+        if axis == 0:
+            for j in range(0, CARD_LENGTH):
+                if not self.bingo_card[line_num][j][True]:
+                    return False
+        else:
+            for i in range(0, CARD_LENGTH):
+                if not self.bingo_card[i][line_num][True]:
+                    return False
+
+        stats.num_line_bingo[axis][line_num] += 1
+        return True
+
+    # ------------------------------------------------------------------------
+
+    def _check_corners_bingo(self, stats):
+
+        if self.bingo_card[0][0][True] and \
+                self.bingo_card[CARD_LENGTH - 1][0][True] and \
+                self.bingo_card[0][CARD_LENGTH - 1][True] and \
+                self.bingo_card[CARD_LENGTH - 1][CARD_LENGTH - 1][True]:
+            stats.num_corners_bingo += 1
+            return True
+
+        return False
+
+    # ------------------------------------------------------------------------
+
+    def check_traditional_bingo(self, stats):
+
+        if self._check_corners_bingo(stats):
+            return True
+
+        for i in range(0, CARD_LENGTH):
+
+            if i in [1, 2] and self._check_diagonal_bingo(i, stats):
+                return True
+
+            if self._check_line_bingo(0, i, stats) or self._check_line_bingo(1, i, stats):
+                return True
+
+        return False
--- bingo_simulator.py
+++ bingo_simulator.py
@@ -1,7 +1,7 @@
 import bingo_card as bc
 
 
-class BingoSimulator:
+class BingoStats:
 
     def __init__(self, num_simulations):
 
@@ -34,120 +34,12 @@ class BingoSimulator:
 
     # ------------------------------------------------------------------------
 
-    def check_diagonal_bingo(self, diag_num, card):
-
-        if diag_num not in [1, 2]:
-            raise ValueError(f'Diagonal Number must be 1 or 2 in check_diagonal_bingo(): {diag_num}.')
-
-        if diag_num == 1:
-            for i in range(0, bc.CARD_LENGTH):
-                if not card[i][i][True]:
-                    return False
-
-        elif diag_num == 2:
-            for i in range(0, bc.CARD_LENGTH):
-                if not card[i][bc.CARD_LENGTH - 1 - i][True]:
-                    return False
-
-        self.num_diag_bingo[diag_num - 1] += 1
-        return True
+    def _print_bingo_result(self, item, num_result):
+        print("{} bingo:  {}, {}%".format(item, num_result, self._compute_percentage(num_result)))
 
     # ------------------------------------------------------------------------
 
-    def check_line_bingo(self, axis, line_num, card):
-
-        if axis not in [0, 1]:
-            raise ValueError(f'Axis is not 0 (row) or 1 (column): {axis}.')
-
-        if line_num not in list(range(0, bc.CARD_LENGTH)):
-            raise ValueError(f'Line number for axis {axis} must be 0-4 in check_line_bingo(): {line_num}.')
-
-        if axis == 0:
-            for j in range(0, bc.CARD_LENGTH):
-                if not card[line_num][j][True]:
-                    return False
-        else:
-            for i in range(0, bc.CARD_LENGTH):
-                if not card[i][line_num][True]:
-                    return False
-
-        self.num_line_bingo[axis][line_num] += 1
-        return True
-
-    # ------------------------------------------------------------------------
-
-    def check_corners_bingo(self, card):
-
-        if card[0][0][True] and \
-                card[bc.CARD_LENGTH - 1][0][True] and \
-                card[0][bc.CARD_LENGTH - 1][True] and \
-                card[bc.CARD_LENGTH - 1][bc.CARD_LENGTH - 1][True]:
-            self.num_corners_bingo += 1
-            return True
-
-        return False
-
-    # ------------------------------------------------------------------------
-
-    def check_traditional_bingo(self, card):
-
-        if self.check_corners_bingo(card):
-            return True
-
-        for i in range(0, bc.CARD_LENGTH):
-
-            if i in [1, 2] and self.check_diagonal_bingo(i, card):
-                return True
-
-            if self.check_line_bingo(0, i, card) or self.check_line_bingo(1, i, card):
-                return True
-
-        return False
-
-    # ------------------------------------------------------------------------
-
-    def play_bingo(self):
-
-        game_card = bc.BingoCard(False)
-
-        # Play bingo
-        random_all = bc.random.sample(game_card.under_all, bc.CARD_LENGTH * bc.COLUMN_RANGE)
-        num_bingo_balls = 0
-        got_bingo = False
-
-        for bingo_ball in random_all:
-
-            if got_bingo:
-                break
-
-            num_bingo_balls += 1
-
-            for i in range(0, bc.CARD_LENGTH):
-
-                if got_bingo:
-                    break
-
-                for j in range(0, bc.CARD_LENGTH):
-                    if game_card.bingo_card[i][j][False] == bingo_ball:
-                        game_card.bingo_card[i][j][True] = True
-                        game_card.marked_bingo_card[i][j][True] = 'X'
-
-                    # Check for bingos
-                    if self.check_traditional_bingo(game_card.bingo_card):
-                        got_bingo = True
-                        self.num_bingo_tries[num_bingo_balls] += 1
-
-                        # print("Numbers called: {}".format(RandomAll[0:num_bingo_balls]))
-                        break
-
-    # ------------------------------------------------------------------------
-
-    def print_bingo_result(self, item, num_result):
-        print("{} bingo:  {}, {}%".format(item, num_result, self.compute_percentage(num_result)))
-
-    # ------------------------------------------------------------------------
-
-    def compute_percentage(self, numerator):
+    def _compute_percentage(self, numerator):
         percentage_factor = float(self.num_simulations / 100.0)
 
         return float(numerator / percentage_factor)
@@ -168,35 +60,80 @@ class BingoSimulator:
         for axis in [0, 1]:
             for i in range(0, bc.CARD_LENGTH):
                 if axis == 0:
-                    self.print_bingo_result(f"{axis_ref[axis]} {i}", self.num_line_bingo[axis][i])
+                    self._print_bingo_result(f"{axis_ref[axis]} {i}", self.num_line_bingo[axis][i])
                 else:
-                    self.print_bingo_result(f"{axis_ref[axis]} {bingo_ref[i]}", self.num_line_bingo[axis][i])
+                    self._print_bingo_result(f"{axis_ref[axis]} {bingo_ref[i]}", self.num_line_bingo[axis][i])
 
                 axis_bingo[axis] += self.num_line_bingo[axis][i]
 
-            self.print_bingo_result(f"{axis_ref[axis]}", axis_bingo[axis])
+            self._print_bingo_result(f"{axis_ref[axis]}", axis_bingo[axis])
             line_bingo += axis_bingo[axis]
 
-        self.print_bingo_result("Line", line_bingo)
+        self._print_bingo_result("Line", line_bingo)
 
         for i in [0, 1]:
-            self.print_bingo_result(f"Diagonal {i+1}", self.num_diag_bingo[i])
+            self._print_bingo_result(f"Diagonal {i + 1}", self.num_diag_bingo[i])
             diag_bingo += self.num_diag_bingo[i]
 
-        self.print_bingo_result("Diagonal", diag_bingo)
+        self._print_bingo_result("Diagonal", diag_bingo)
 
-        self.print_bingo_result("Corners", self.num_corners_bingo)
+        self._print_bingo_result("Corners", self.num_corners_bingo)
 
         print(self.num_bingo_tries)
 
+    # ------------------------------------------------------------------------
 
-# ------------------------------------------------------------------------
+
+class BingoSimulator:
+
+    def __init__(self, num_simulations):
+
+        self.stats = BingoStats(num_simulations)
+
+    # ------------------------------------------------------------------------
+
+    def play_bingo(self, free_cell):
+
+        for _ in range(self.stats.num_simulations):
+
+            game_card = bc.BingoCard(free_cell)
+
+            # Play bingo
+            random_all = bc.random.sample(game_card.under_all, bc.CARD_LENGTH * bc.COLUMN_RANGE)
+            num_bingo_balls = 0
+            got_bingo = False
+
+            for bingo_ball in random_all:
+
+                if got_bingo:
+                    break
+
+                num_bingo_balls += 1
+
+                for i in range(0, bc.CARD_LENGTH):
+
+                    if got_bingo:
+                        break
+
+                    for j in range(0, bc.CARD_LENGTH):
+                        if game_card.bingo_card[i][j][False] == bingo_ball:
+                            game_card.bingo_card[i][j][True] = True
+
+                        # Check for bingos
+                        if game_card.check_traditional_bingo(self.stats):
+                            got_bingo = True
+                            self.stats.num_bingo_tries[num_bingo_balls] += 1
+
+                            # print("Numbers called: {}".format(RandomAll[0:num_bingo_balls]))
+                            break
+
+    # ------------------------------------------------------------------------
+
 
 if __name__ == '__main__':
 
     bingo_game_sim = BingoSimulator(1000)
 
-    for _ in range(bingo_game_sim.num_simulations):
-        bingo_game_sim.play_bingo()
+    bingo_game_sim.play_bingo(False)
 
-    bingo_game_sim.print_summary()
+    bingo_game_sim.stats.print_summary()
