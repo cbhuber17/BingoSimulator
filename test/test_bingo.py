43d2030b4b0f7ce29d0b5978cc445c3d2d129377
Colin Huber <cbhuber@gmail.com>
2022-06-06 20:14:39 -0600

    test: Added additional row/col tests and corners unit tests.

--- test/test_bingo.py
+++ test/test_bingo.py
@@ -9,9 +9,14 @@ PACKAGE_NAME = "bingo_simulator"
 # Helper functions for unit testing
 # -------------------------------------------------------------------------------------------------------------
 
+def get_card_and_stats():
+    return BingoCard(False), BingoStats(1)
+
+
+# -------------------------------------------------------------------------------------------------------------
+
 def get_valid_diagonal1():
-    card = BingoCard(False)
-    stats = BingoStats(1)
+    card, stats = get_card_and_stats()
 
     for i in range(0, CARD_LENGTH):
         card.bingo_card[i][i][True] = True
@@ -22,8 +27,7 @@ def get_valid_diagonal1():
 # -------------------------------------------------------------------------------------------------------------
 
 def get_valid_diagonal2():
-    card = BingoCard(False)
-    stats = BingoStats(1)
+    card, stats = get_card_and_stats()
 
     for i in range(0, CARD_LENGTH):
         card.bingo_card[i][CARD_LENGTH - 1 - i][True] = True
@@ -35,8 +39,7 @@ def get_valid_diagonal2():
 
 
 def get_valid_row(row_num):
-    card = BingoCard(False)
-    stats = BingoStats(1)
+    card, stats = get_card_and_stats()
 
     for i in range(0, CARD_LENGTH):
         card.bingo_card[row_num][i][True] = True
@@ -47,11 +50,24 @@ def get_valid_row(row_num):
 # -------------------------------------------------------------------------------------------------------------
 
 def get_valid_col(col_num):
-    card = BingoCard(False)
-    stats = BingoStats(1)
+    card, stats = get_card_and_stats()
 
     for i in range(0, CARD_LENGTH):
         card.bingo_card[i][col_num][True] = True
+
+    return card, stats
+
+
+# -------------------------------------------------------------------------------------------------------------
+
+def get_valid_corners():
+    card, stats = get_card_and_stats()
+    corner_index = CARD_LENGTH - 1
+
+    card.bingo_card[0][0][True] = True
+    card.bingo_card[corner_index][0][True] = True
+    card.bingo_card[0][corner_index][True] = True
+    card.bingo_card[corner_index][corner_index][True] = True
 
     return card, stats
 
@@ -114,7 +130,6 @@ def test_bad_diagonal_two(card_and_stat):
 
 # -------------------------------------------------------------------------------------------------------------
 
-
 good_rows = []
 good_row_stats = []
 line_num = []
@@ -154,5 +169,89 @@ def test_valid_cols(cards_and_stats_and_line_num):
                                                              cards_and_stats_and_line_num[1]) \
            and cards_and_stats_and_line_num[1].num_line_bingo[1][cards_and_stats_and_line_num[2]] == 1
 
+
 # -------------------------------------------------------------------------------------------------------------
 
+bad_rows = []
+bad_row_stats = []
+line_num = []
+
+for bad_col in range(0, CARD_LENGTH):
+    for i in range(0, CARD_LENGTH):
+        card, stats = get_valid_row(i)
+        card.bingo_card[i][bad_col][True] = False
+        bad_rows.append(card)
+        bad_row_stats.append(stats)
+        line_num.append(i)
+
+cards_and_stats_and_line_nums = list(zip(bad_rows, bad_row_stats, line_num))
+
+
+@pytest.mark.parametrize('cards_and_stats_and_line_num', cards_and_stats_and_line_nums)
+def test_invalid_rows(cards_and_stats_and_line_num):
+    assert not cards_and_stats_and_line_num[0]._check_line_bingo(0, cards_and_stats_and_line_num[2],
+                                                                 cards_and_stats_and_line_num[1]) \
+           and cards_and_stats_and_line_num[1].num_line_bingo[0][cards_and_stats_and_line_num[2]] == 0
+
+
+# -------------------------------------------------------------------------------------------------------------
+
+bad_cols = []
+bad_col_stats = []
+line_num = []
+
+for bad_row in range(0, CARD_LENGTH):
+    for i in range(0, CARD_LENGTH):
+        card, stats = get_valid_col(i)
+        card.bingo_card[bad_row][i][True] = False
+        bad_cols.append(card)
+        bad_col_stats.append(stats)
+        line_num.append(i)
+
+cards_and_stats_and_line_nums = list(zip(bad_cols, bad_col_stats, line_num))
+
+
+@pytest.mark.parametrize('cards_and_stats_and_line_num', cards_and_stats_and_line_nums)
+def test_invalid_cols(cards_and_stats_and_line_num):
+    assert not cards_and_stats_and_line_num[0]._check_line_bingo(1, cards_and_stats_and_line_num[2],
+                                                                 cards_and_stats_and_line_num[1]) \
+           and cards_and_stats_and_line_num[1].num_line_bingo[1][cards_and_stats_and_line_num[2]] == 0
+
+
+# -------------------------------------------------------------------------------------------------------------
+
+def test_valid_corners():
+    card, stats = get_valid_corners()
+    assert card._check_corners_bingo(stats) and stats.num_corners_bingo == 1
+
+
+# -------------------------------------------------------------------------------------------------------------
+
+def test_invalid_corner1():
+    card, stats = get_valid_corners()
+    card.bingo_card[0][0][True] = False
+    assert not card._check_corners_bingo(stats) and stats.num_corners_bingo == 0
+
+
+# -------------------------------------------------------------------------------------------------------------
+
+def test_invalid_corner2():
+    card, stats = get_valid_corners()
+    card.bingo_card[CARD_LENGTH - 1][0][True] = False
+    assert not card._check_corners_bingo(stats) and stats.num_corners_bingo == 0
+
+
+# -------------------------------------------------------------------------------------------------------------
+
+def test_invalid_corner3():
+    card, stats = get_valid_corners()
+    card.bingo_card[0][CARD_LENGTH - 1][True] = False
+    assert not card._check_corners_bingo(stats) and stats.num_corners_bingo == 0
+
+
+# -------------------------------------------------------------------------------------------------------------
+
+def test_invalid_corner4():
+    card, stats = get_valid_corners()
+    card.bingo_card[CARD_LENGTH - 1][CARD_LENGTH - 1][True] = False
+    assert not card._check_corners_bingo(stats) and stats.num_corners_bingo == 0
